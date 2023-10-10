738516203d3abd8a2f573677b2b630bee40e8064
Joel Lozano <joel.lozano@rappi.com>
2023-10-09 19:29:26 -0600

    FIJE JSON

--- Proyecto/JS/index.js
+++ Proyecto/JS/index.js
@@ -1,10 +1,30 @@
 // Gestion de Alumno 
 
+// Leer datos de un archivo externo JSON 
+/* La funcion fetch sirve para consultar datos / informacion de una API o un archivo
+para su uso debo usar una promesa, la cual me permite ejecutar codigo cuando la promesa se cumpla.
+*/ 
+
+
+let data = [];
+
+fetch('./File.json')
+.then(response => {
+    if (!response.ok) {
+        throw Error("Error")
+    }
+    return response.json();
+}).then(data => {
+    console.log(data)
+}).catch(error => {
+    console.log(error)
+})
+
 
 class Grupo {
-    constructor(nombre, alumnos) {
+    constructor(nombre, data) {
         this.nombre = nombre
-        this.alumnos = alumnos
+        this.data = data
     }
 }
 
@@ -15,34 +35,16 @@ class Alumno {
     }
 }
 
-let alumno1 = new Alumno("Erick", 10)
-let alumno2 = new Alumno("Luis", 8)
-let alumno3 = new Alumno("Juan", 9)
-let alumno4 = new Alumno("Maria", 7)
-let alumno5 = new Alumno("Ana", 6)
-let alumno6 = new Alumno("Pedro", 5)
-let alumno7 = new Alumno("Jose", 4)
-let alumno8 = new Alumno("Carlos", 3)
-let alumno9 = new Alumno("Ricardo", 2)
-let alumno10 = new Alumno("Fernando", 1)
-let alumno11 = new Alumno("Alfonso", 10)
-let alumno12 = new Alumno("Luisa", 8)
-let alumno13 = new Alumno("Katia", 9)
-let alumno14 = new Alumno("Mariana", 7)
-let alumno15 = new Alumno("Anabarbara", 6)
-let alumno16 = new Alumno("Pedro", 5)
 
-
-var alumnos = [alumno1, alumno2, alumno3, alumno4, alumno5, alumno6, alumno7, alumno8, alumno9, alumno10, alumno11, alumno12, alumno13, alumno14, alumno15, alumno16]
-let grupo1 = new Grupo("Grupo 1", alumnos)
+let grupo1 = new Grupo("Grupo 1", data.data)
 
 
 function agregarAlumno() {
     let nombre = document.getElementById("nombre").value
     let calificacion = document.getElementById("calificacion").value
     let nuevoAlumno = new Alumno(nombre, calificacion)
-    alumnos.push(nuevoAlumno)
-    mostrarAlumnos(alumnos)
+    data.push(nuevoAlumno)
+    mostrarAlumnos(data)
 }
 
 function mostrarAlumnos(array) {
@@ -69,18 +71,18 @@ function limpiarTabla() {
 }
 
 function eliminarAlumno(indice) {
-   alumnos.splice(indice, 1)
+   data.splice(indice, 1)
    document.getElementById("alertContainer").style.display = "block"
-    mostrarAlumnos(alumnos)
+    mostrarAlumnos(data)
 }
 
 function eliminarUltimoRegistro() {
-    alumnos.pop()
-    mostrarAlumnos(alumnos)
+    data.pop()
+    mostrarAlumnos(data)
 }
 
 function ordernarAlfabeticamente() {
-    let arrayOrdenado = alumnos.sort((a, b) => {
+    let arrayOrdenado = data.sort((a, b) => {
         if (a.nombre > b.nombre) {
             return 1
         }
@@ -95,7 +97,7 @@ function ordernarAlfabeticamente() {
 function buscarAlumno() {
     let alumnoAbuscar = document.getElementById("serchStudent").value
     const nombreCapitalizado = alumnoAbuscar.charAt(0).toUpperCase() + alumnoAbuscar.slice(1)
-    let arrayAlumnoFiltrado = alumnos.filter( alumno => alumno.nombre.includes(nombreCapitalizado))
+    let arrayAlumnoFiltrado = data.filter( alumno => alumno.nombre.includes(nombreCapitalizado))
     if (arrayAlumnoFiltrado.length == 0) {
         alert("No se encontro el alumno")
     } else {
@@ -105,9 +107,17 @@ function buscarAlumno() {
 }
 
 // mostrarAlumnos
-mostrarAlumnos(alumnos)
+mostrarAlumnos(data)
 
 
 // leer datos del input con el evento input
  let searchInput = document.getElementById("serchStudent");
     searchInput.addEventListener("input", buscarAlumno)
+
+
+   var objeto =  {
+        nombre: "joel",
+        apellido: "lozano",
+        edad: 12,
+        calificaciones : [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    }
